307153f15a85e3cbcd5f491bd259f63fcd952744
Arthur Fewell <afewell@gmail.com>
2023-04-24 23:10:59 -0700

    Update aws-vpc_tanzu-ra.sh

--- pipelines/aws-vpc_tanzu-ra/aws-vpc_tanzu-ra.sh
+++ pipelines/aws-vpc_tanzu-ra/aws-vpc_tanzu-ra.sh
@@ -1,4 +1,4 @@
-# printing 50 lines to help compensate for codestream bug that omits a semi-random number of output from the beginnings of its logs.
+# printing 50 lines to compensate for codestream bug that omits a semi-random number of output from the beginnings of its logs.
 for i in $(seq 1 50); do
     echo "placeholder line $i"
 done
@@ -50,16 +50,24 @@ export ACCT_FILE="$(realpath creds.yaml)"
 wget https://raw.githubusercontent.com/genaissance/aria_auto/main/pipelines/aws-vpc_tanzu-ra/upload_file_github.py
 # download init.sls file used to create/deploy defined objects in file
 wget https://raw.githubusercontent.com/genaissance/aria_auto/main/pipelines/aws-vpc_tanzu-ra/init.template.sls
+# download create_guardrails.py
+wget https://raw.githubusercontent.com/genaissance/aria_auto/main/pipelines/aws-vpc_tanzu-ra/create_guardrails.py
+# download reorder.py
+wget https://raw.githubusercontent.com/genaissance/aria_auto/main/pipelines/aws-vpc_tanzu-ra/reorder.py
+# Install any dependent packages in python scripts
+pip install pipreqs
+pipreqs .
+pip install -r requirements.txt
 # populate the init.sls template with local envars
 envsubst < init.template.sls > init.sls
 echo "Here is the init.sls file with envars populated:"
 # run idem state on the init.sls to create the defined objects
 ## specify the output value as yaml and redirect output to deployment_guardrails.sls file
 idem state init.sls --output yaml > init.output.yaml
-deployment_guardrails.sls
-pip install pipreqs
-pipreqs .
-pip install -r requirements.txt
+# convert init.output.yaml to unordered_guardrails.sls using create_guardrails.py
+python3 create_guardrails.py init.output.yaml unordered_guardrails.sls
+# call reorder.py to reorder the unordered_guardrails.sls file and save as deployment_guardrails.sls
+python3 reorder.py init.sls unordered_guardrails.sls deployment_guardrails.sls
 python3 upload_file_github.py "$your_github_token" "$repo_owner_or_org_name" "$private_repo_name" "deployment_guardrails.sls" "pipelines/aws-vpc_tanzu-ra/artifacts/$PIPELINE_START_TIMESTAMP/deployment_guardrails.sls"
 python3 upload_file_github.py "$your_github_token" "$repo_owner_or_org_name" "$private_repo_name" "init.sls" "pipelines/aws-vpc_tanzu-ra/artifacts/$PIPELINE_START_TIMESTAMP/init.sls"
 
