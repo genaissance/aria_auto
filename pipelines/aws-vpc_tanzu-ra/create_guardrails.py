07d63433e7851c04e404c36c3310a470424a9cd7
Arthur Fewell <afewell@gmail.com>
2023-04-25 01:41:54 -0700

    Update create_guardrails.py

--- pipelines/aws-vpc_tanzu-ra/create_guardrails.py
+++ pipelines/aws-vpc_tanzu-ra/create_guardrails.py
@@ -12,14 +12,11 @@ def reformat_yaml(input_file, output_file):
         name = value['name']
         esm_tag = key.split('_|', 1)[0] + '.present'
         new_data[name] = {
-            esm_tag: {}
+            esm_tag: []
         }
-        for k, v in value['new_state'].items():
-            if k not in {'name', 'resource_id'}:
-                new_data[name][esm_tag][k] = v
 
         if 'resource_id' in value['new_state']:
-            new_data[name][esm_tag]['resource_id'] = value['new_state']['resource_id']
+            new_data[name][esm_tag].append({'resource_id': value['new_state']['resource_id']})
 
     with open(output_file, 'w') as outfile:
         yaml.dump(new_data, outfile)
