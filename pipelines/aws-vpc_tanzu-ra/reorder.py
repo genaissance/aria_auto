4c337f26301ad577650de226cf30cf6d68906518
Arthur Fewell <afewell@gmail.com>
2023-04-25 03:35:26 -0700

    Update reorder.py

--- pipelines/aws-vpc_tanzu-ra/reorder.py
+++ pipelines/aws-vpc_tanzu-ra/reorder.py
@@ -1,47 +1,31 @@
-# The purpose of this file is to change the order of objects in a target sls file to match the order of objects defined in an ordered file path. The ordered_file_path file should be a txt file that can be generated from the init.sls file with the command `grep "^[[:alnum:]]" init.sls > object_order.txt`. The target sls file should only include object definitions and not include any parameter definitions.
-# Usage: python3 reorder.py <ordered_file> <unordered_file> <output_file>
 import sys
 import os
+import re
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip().split('\n')
 
 def main(ordered_file_path, unordered_file_path, output_file_path):
-    ordered_keys = read_file(ordered_file_path)
+    ordered_lines = read_file(ordered_file_path)
     unordered_lines = read_file(unordered_file_path)
 
-    unordered_dict = {}
-    current_key = None
+    ordered_object_names = [re.match(r'^([a-zA-Z0-9_]+):', line).group(1) for line in ordered_lines if re.match(r'^([a-zA-Z0-9_]+):', line)]
+    unordered_objects = {}
+
+    current_object = None
     for line in unordered_lines:
-        if line in ordered_keys:
-            current_key = line
-            unordered_dict[current_key] = []
-        else:
-            unordered_dict[current_key].append(line)
-
-    temp_output_file_path = 'temp_output.txt'
-
-    with open(temp_output_file_path, 'w') as temp_output_file:
-        for key in ordered_keys:
-            if key in unordered_dict:
-                temp_output_file.write(key + '\n')
-                for line in unordered_dict[key]:
-                    temp_output_file.write(line + '\n')
-                temp_output_file.write('\n')
-
-    # Read the temporary output file and remove blank lines
-    with open(temp_output_file_path, 'r') as temp_output_file:
-        lines = temp_output_file.readlines()
+        if re.match(r'^([a-zA-Z0-9_]+):', line):
+            current_object = re.match(r'^([a-zA-Z0-9_]+):', line).group(1)
+            unordered_objects[current_object] = [line]
+        elif current_object is not None:
+            unordered_objects[current_object].append(line)
 
     with open(output_file_path, 'w') as output_file:
-        for line in lines:
-            if line.strip():
-                output_file.write(line)
-
-    # Remove the temporary output file
-    os.remove(temp_output_file_path)
-
+        for ordered_object_name in ordered_object_names:
+            if ordered_object_name in unordered_objects:
+                output_file.write('\n'.join(unordered_objects[ordered_object_name]))
+                output_file.write('\n\n')
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
