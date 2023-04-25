87f9a954baca57c54359ec9df7bd6f01e424a705
Arthur Fewell <afewell@gmail.com>
2023-04-24 22:47:36 -0700

    Update create_guardrails.py

--- pipelines/aws-vpc_tanzu-ra/create_guardrails.py
+++ pipelines/aws-vpc_tanzu-ra/create_guardrails.py
@@ -1,36 +1,30 @@
-import yaml
 import sys
+import ruamel.yaml
 
-def main(init_sls, init_output, output_filename):
-    # Load the contents of init_sls
-    with open(init_sls, "r") as file1:
-        file1_data = yaml.safe_load(file1)
+def reformat_yaml(input_file, output_file):
+    yaml = ruamel.yaml.YAML()
+    with open(input_file, 'r') as infile:
+        data = yaml.load(infile)
 
-    # Load the contents of init_output
-    with open(init_output, "r") as file2:
-        file2_data = yaml.safe_load(file2)
+    new_data = {}
 
-    # Update the objects in file1_data with the "new_state" values from file2_data
-    for key in file1_data:
-        # Find the corresponding key in file2_data
-        for file2_key in file2_data:
-            if file2_data[file2_key].get("__id__") == key:
-                # Update the object in file1_data with the "new_state" values from file2_data
-                for new_state_key in file2_data[file2_key]["new_state"]:
-                    file1_data[key][new_state_key] = file2_data[file2_key]["new_state"][new_state_key]
-                break
+    for key, value in data.items():
+        name = value['name']
+        esm_tag = key.split('_|', 1)[0] + '.present'
+        new_data[name] = {
+            esm_tag: {}
+        }
+        for k, v in value['new_state'].items():
+            if k not in {'name', 'resource_id'}:
+                new_data[name][esm_tag][k] = v
 
-    # Save the updated init.sls
-    with open(output_filename, "w") as updated_file1:
-        yaml.dump(file1_data, updated_file1)
+        if 'resource_id' in value['new_state']:
+            new_data[name][esm_tag]['resource_id'] = value['new_state']['resource_id']
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: create_guardrails.py <init.sls> <init.output.yaml> <output_filename>")
-        sys.exit(1)
+    with open(output_file, 'w') as outfile:
+        yaml.dump(new_data, outfile)
 
-    init_sls = sys.argv[1]
-    init_output = sys.argv[2]
-    output_filename = sys.argv[3]
-
-    main(init_sls, init_output, output_filename)
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    reformat_yaml(input_file, output_file)
